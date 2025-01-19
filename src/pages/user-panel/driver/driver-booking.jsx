78dee0bfff2ad44f2a9a63bb5e6195fb386eadba
unknown <qaziabdurrahman12@gmail.com>
2025-01-19 01:56:45 -0800

    my changes

--- src/pages/user-panel/driver/driver-booking.jsx
+++ src/pages/user-panel/driver/driver-booking.jsx
@@ -139,7 +139,6 @@ const DriverBooking = () => {
             ))}
           </div>
         )}
-=======
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 justify-center">
           {Array.from({ length: 12 }).map((_, index) => (
             <DriverCard
